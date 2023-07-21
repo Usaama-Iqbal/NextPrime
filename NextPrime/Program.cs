1b60919f43a39be645968eaa546362f521a59e85
Usama Iqbal <uiqbal@cedrus.com>
2023-07-21 12:39:50 +0500

    feat : you can give the program prime number this program tells you the next prime number

---
+++ NextPrime/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+public class prime
+{
+    public static void Main(string[] args)
+    {
+        int N ;
+        Console.Write("Please enter a prime number : ");
+        N = Convert.ToInt32(Console.ReadLine());
+        for (int nextPrime = N + 1; ; nextPrime++)
+        {
+            if (IsPrime(nextPrime))
+            {
+                Console.Write("The Next Prime Number Is : "+nextPrime);
+                break;
+            }
+        }
+        static bool IsPrime(int n)
+        {
+            if (n <= 1)
+                return false;
+            if (n <= 3)
+                return true;
+
+            if (n % 2 == 0 || n % 3 == 0)
+                return false;
+
+            for (int i = 5; i * i <= n; i += 6)
+            {
+                if (n % i == 0 || n % (i + 2) == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+
+    //static bool IsPrime(int n)
+    //{
+    //    if (n <= 1)
+    //        return false;
+    //    if (n <= 3) 
+    //        return true;
+
+    //    if (n % 2 == 0 || n % 3 == 0)
+    //        return false;
+
+    //    for (int i = 5; i * i <= n; i += 6)
+    //    {
+    //        if (n % i == 0 || n % (i + 2) == 0)
+    //            return false;
+    //    }
+
+    //    return true;
+    //}
+}
